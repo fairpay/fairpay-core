dfe7d5b11d5f220a48a54797f401e595c8a8eb15
Nick Keller <solix@hotmail.fr>
2016-04-06 22:04:57 +0200

    Update usermanager tests

--- src/Fairpay/Bundle/UserBundle/Tests/Manager/UserManagerTest.php
+++ src/Fairpay/Bundle/UserBundle/Tests/Manager/UserManagerTest.php
@@ -7,16 +7,25 @@ namespace Fairpay\Bundle\UserBundle\Tests\Manager;
 use Fairpay\Bundle\SchoolBundle\Entity\School;
 use Fairpay\Bundle\UserBundle\Entity\User;
 use Fairpay\Bundle\UserBundle\Manager\UserManager;
-use Fairpay\Util\Manager\NoCurrentSchoolException;
-use Fairpay\Util\Tests\WebTestCase;
+use Fairpay\Util\Tests\UnitTestCase;
+use Fairpay\Util\Util\StringUtil;
+use Fairpay\Util\Util\TokenGenerator;
+use Prophecy\Argument;
 
-class UserManagerTest extends WebTestCase
+class UserManagerTest extends UnitTestCase
 {
+    const user_repository       = 'Fairpay\Bundle\UserBundle\Repository\UserRepository';
+    const user_password_encoder = 'Symfony\Component\Security\Core\Encoder\UserPasswordEncoder';
+
     /** @var  UserManager */
     public $userManager;
 
-    /** @var  School */
-    public $school;
+    // Mocked
+    private $em;
+    private $repo;
+    private $dispatcher;
+    private $passwordEncoder;
+    private $schoolManager;
 
     /**
      * {@inheritDoc}
@@ -24,159 +33,168 @@ class UserManagerTest extends WebTestCase
     protected function setUp()
     {
         parent::setUp();
-        $this->userManager = $this->container->get('user_manager');
-        $this->school = null;
+
+        $this->passwordEncoder = $this->mock(self::user_password_encoder);
+
+        $this->userManager = new UserManager(
+            $this->passwordEncoder->reveal(),
+            new TokenGenerator(),
+            new StringUtil()
+        );
+
+        $this->em = $this->mock(self::doctrine_orm_entity_manager);
+        $this->dispatcher = $this->mock(self::event_dispatcher);
+        $this->userManager->init($this->em->reveal(), $this->dispatcher->reveal());
+
+        $this->schoolManager = $this->mock(self::school_manager);
+        $this->schoolManager->getCurrentSchool()->willReturn(null);
+        $this->userManager->setSchoolManager($this->schoolManager->reveal());
+
+        $this->repo = $this->mock(self::user_repository);
+        $this->em->getRepository(UserManager::ENTITY_SHORTCUT_NAME)->willReturn($this->repo->reveal());
     }
 
     public function isEmailProvider()
     {
-        return [[array(
+        return [
             ['username', false],
             ['email@domain', true],
-        )]];
+        ];
     }
 
     /**
      * @dataProvider isEmailProvider
-     * @param $data
+     * @param $email
+     * @param $expected
      */
-    public function testIsEmail($data)
+    public function testIsEmail($email, $expected)
     {
-        foreach ($data as list($email, $expected)) {
-            $this->assertEquals($expected, $this->userManager->isEmail($email));
-        }
+        $this->assertEquals($expected, $this->userManager->isEmail($email));
+    }
+
+    /**
+     * @expectedException Fairpay\Util\Manager\NoCurrentSchoolException
+     */
+    public function testFindUserByIdWithoutSchool()
+    {
+        $this->userManager->findUserById(42);
     }
 
     public function testFindUserById()
     {
+        $this->havingAUser();
         $this->havingASchool();
-        $user = $this->havingAUser();
 
-        try {
-            $this->userManager->findUserById($user->getId());
-            $this->fail('Should not be able to find a user without having a current School defined.');
-        } catch (NoCurrentSchoolException $e) {
-            // Should throw an exception when no current School is defined.
-        }
+        $user = $this->userManager->findUserById(42);
 
-        // Should work when current school is defined
-        $this->container->get('school_manager')->setCurrentSchool($this->school);
-        $this->assertEquals($user->getId(), $this->userManager->findUserById($user->getId())->getId());
+        $this->assertNotNull($user);
+        $this->schoolManager->getCurrentSchool()->shouldHaveBeenCalled();
+    }
 
-        // Should not find users from other schools
-        $this->havingASchool();
-        $user = $this->havingAUser();
-        $this->assertNull($this->userManager->findUserById($user->getId()));
+    /**
+     * @expectedException Fairpay\Util\Manager\NoCurrentSchoolException
+     */
+    public function testFindUserByUserNameOrEmailWithoutSchool()
+    {
+        $this->userManager->findUserByUsernameOrEmail('username');
     }
 
     public function testFindUserByUserNameOrEmail()
     {
+        $this->havingAUser();
         $this->havingASchool();
-        $user = $this->havingAUser('username', 'email@domain');
 
-        try {
-            $this->userManager->findUserByUsernameOrEmail($user->getUsername());
-            $this->fail('Should not be able to find a user without having a current School defined.');
-        } catch (NoCurrentSchoolException $e) {
-            // Should throw an exception when no current School is defined.
-        }
+        $this->userManager->findUserByUsernameOrEmail('username');
+        $this->repo->findByUsername(Argument::type(School::class), 'username')->shouldHaveBeenCalled();
 
-        // Should work when current school is defined
-        $this->container->get('school_manager')->setCurrentSchool($this->school);
-        $this->assertEquals($user->getId(), $this->userManager->findUserByUsernameOrEmail($user->getUsername())->getId());
-        $this->assertEquals($user->getId(), $this->userManager->findUserByUsernameOrEmail($user->getEmail())->getId());
+        $this->userManager->findUserByUsernameOrEmail('email@domain');
+        $this->repo->findByEmail(Argument::type(School::class), 'email@domain')->shouldHaveBeenCalled();
+    }
 
-        // Should not find users from other schools
-        $this->havingASchool();
-        $user = $this->havingAUser('other_name', 'other@email');
-        $this->assertNull($this->userManager->findUserByUsernameOrEmail($user->getUsername()));
-        $this->assertNull($this->userManager->findUserByUsernameOrEmail($user->getEmail()));
+    /**
+     * @expectedException Fairpay\Util\Manager\NoCurrentSchoolException
+     */
+    public function testCreateUserWithoutSchool()
+    {
+        $this->userManager->createMainVendor('Bruce Wayne', 'b4atman', 'bruce@wayne');
     }
 
     public function testCreateUser()
     {
         $this->havingASchool();
+        $this->havingTakenUsernames();
 
-        try {
-            $this->userManager->createMainVendor('Bruce Wayne', 'b4atman', 'bruce@wayne');
-            $this->fail('Should not be able to find a user without having a current School defined.');
-        } catch (NoCurrentSchoolException $e) {
-            // Should throw an exception when no current School is defined.
-        }
+        $this->em->persist(Argument::type(User::class))->shouldBeCalled();
+        $this->em->flush()->shouldBeCalled();
+        $this->passwordEncoder->encodePassword(Argument::type(User::class), 'b4atman')->willReturn('encoded_password');
 
-        // Should work when current school is defined
-        $this->container->get('school_manager')->setCurrentSchool($this->school);
         $user = $this->userManager->createMainVendor('Bruce Wayne', 'b4atman', 'bruce@wayne');
 
-        $this->assertNotNull($user->getId());
         $this->assertEquals('bruce.wayne', $user->getUsername());
         $this->assertEquals('Bruce Wayne', $user->getDisplayName());
         $this->assertEquals('bruce@wayne', $user->getEmail());
+        $this->assertEquals('encoded_password', $user->getPassword());
+        $this->assertEquals(true, $user->getIsVendor());
         $this->assertNotNull($user->getSalt());
-        $this->assertNotNull($user->getPassword());
-        $this->assertNotEquals('b4atman', $user->getPassword());
     }
 
-    public function testUsernameFromDisplayName()
+    /**
+     * @expectedException Fairpay\Util\Manager\NoCurrentSchoolException
+     */
+    public function testUsernameFromDisplayNameWithoutSchool()
+    {
+        $this->userManager->usernameFromDisplayName('Bruce Wayne');
+    }
+
+    public function usernameFromDisplayNameProvider()
+    {
+        return [
+            [[], 'Only Username', 'only.username'],
+            [['username'], 'Username!', 'username1'],
+            [['username', 'username1', 'username2', 'username4'], 'username', 'username3'],
+        ];
+    }
+
+    /**
+     * @dataProvider usernameFromDisplayNameProvider
+     * @param $takenUsernames
+     * @param $displayName
+     * @param $expected
+     */
+    public function testUsernameFromDisplayName($takenUsernames, $displayName, $expected)
     {
         $this->havingASchool();
-        $this->havingAUser('username');
-        $this->havingAUser('username_again');
+        $this->havingTakenUsernames($takenUsernames);
 
-        try {
-            $this->userManager->usernameFromDisplayName('Bruce Wayne');
-            $this->fail('Should not be able to find a user without having a current School defined.');
-        } catch (NoCurrentSchoolException $e) {
-            // Should throw an exception when no current School is defined.
-        }
+        $username = $this->userManager->usernameFromDisplayName($displayName);
 
-        $this->container->get('school_manager')->setCurrentSchool($this->school);
-
-        $this->assertEquals('only.username', $this->userManager->usernameFromDisplayName('Only Username'));
-        $this->assertEquals('username1', $this->userManager->usernameFromDisplayName('Username!'));
-
-        $this->havingAUser('username1');
-        $this->havingAUser('username2');
-        $this->havingAUser('username4');
-
-        $this->assertEquals('username3', $this->userManager->usernameFromDisplayName('username'));
+        $this->assertEquals($expected, $username);
     }
 
     /**
-     * Create a School in the DB.
-     * @param $name
-     * @param $email
-     * @param $slug
+     * Current School is set.
      */
-    protected function havingASchool($name = null, $email = null, $slug = null)
+    protected function havingASchool()
     {
-        $school = new School($name ? $name : uniqid(), $email ? $email : uniqid());
-        $school->setSlug($slug ? $slug : uniqid());
-
-        $this->em->persist($school);
-        $this->em->flush();
-
-        $this->school = $school;
+        $this->schoolManager->getCurrentSchool()->willReturn(new School());
     }
 
     /**
-     * Create a School in the DB.
-     * @param $username
-     * @return User
+     * findUserById, findByEmail, and findByUsername wil return a User.
      */
-    public function havingAUser($username = null, $email = null)
+    public function havingAUser()
     {
-        $user = new User();
-        $user->setDisplayName(uniqid());
-        $user->setSalt(uniqid());
-        $user->setPassword(uniqid());
-        $user->setEmail($email ? $email : uniqid());
-        $user->setSchool($this->school);
-        $user->setUsername($username ? $username : uniqid());
+        $this->repo->findUserById(Argument::type(School::class), Argument::any())->willReturn(new User());
+        $this->repo->findByEmail(Argument::type(School::class), Argument::any())->willReturn(new User());
+        $this->repo->findByUsername(Argument::type(School::class), Argument::any())->willReturn(new User());
+    }
 
-        $this->em->persist($user);
-        $this->em->flush();
-
-        return $user;
+    /**
+     * findTakenUsernames will return the $usernames.
+     * @param array $usernames
+     */
+    public function havingTakenUsernames(array $usernames = [])
+    {
+        $this->repo->findTakenUsernames(Argument::type(School::class), Argument::any())->willReturn($usernames);
     }
 }
